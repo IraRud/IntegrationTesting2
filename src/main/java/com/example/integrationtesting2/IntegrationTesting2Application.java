3d3afb11de5eeb60707678a012cc6d545e6388d2
rudskaya irina <irarudskaya5@gmail.com>
2023-11-09 00:16:04 +0300

    Initial commit

---
+++ src/main/java/com/example/integrationtesting2/IntegrationTesting2Application.java
@@ -0,0 +1,13 @@
+package com.example.integrationtesting2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationTesting2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationTesting2Application.class, args);
+	}
+
+}
